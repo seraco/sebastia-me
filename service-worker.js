e44f19fabe2ae88859872497f1be78ea77465386
seraco <sebastiaramon_1994@hotmail.com>
2018-12-31 10:10:58 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a68af4ab3ffe04b23c6bd6822d7a3d7f.js"
+  "/precache-manifest.890c14d83a582b1c821dcdb075f1e1ce.js"
 );
 
 workbox.clientsClaim();
