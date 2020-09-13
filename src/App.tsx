8aac431ed0df03bd95ddb83c9904533e7a6cc1ff
Sebastia Ramon <sebastiaramon_1994@hotmail.com>
2020-09-14 01:04:05 +0200

    change favicon; update dependencies; modify text, remove unnecessary sections and images

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,13 @@ interface IState {
   height: number;
 }
 
+const links = {
+  'corrux': <Link href="https://corrux.io">Corrux</Link>,
+  'open-cosmos': <Link href="https://open-cosmos.com">Open Cosmos</Link>,
+  'react': <Link href="https://ca.reactjs.org">React</Link>,
+  'wetipp': <Link href="https://www.wetipp.com">WeTipp</Link>,
+};
+
 class App extends Component {
   state: IState;
 
@@ -42,43 +49,15 @@ class App extends Component {
           title="Hey! My name is Seb"
           src="seb.jpg"
           alt="seb">
-          I am an Aerospace Engineer and Software Developer. I studied Aerospace
-          Engineering at the <Link href="https://www.upc.edu/en">Polytechnic
-          University of Catalonia</Link>. During my studies, I moved abroad to
-          learn more about different cultures and approaches to teaching. As an
-          Erasmus student, I studied at
-          the <Link href="https://www.polimi.it/en/">Polytechnic University of
-          Milan</Link>; I later undertook my MSc
-          at <Link href="https://www.imperial.ac.uk/">Imperial College
-          London</Link>.
+          I'm a Software Developer and Aerospace Engineer.
+          <br /><br />
+          Currently working at {links['corrux']}. Previously,
+          at {links['wetipp']} and {links['open-cosmos']}. I maintain
+          the {links['react']} website translation into Catalan.
+          In my free time, I love climbing, hiking and skiing. If you want
+          to find me, I'll probably be in the mountains.
         </Section>
-        <Section
-          order={orient === 'landscape' ? 1 : 0}
-          src="coding.jpeg"
-          alt="coding">
-          I love coding! In parallel to my university studies, I expanded my
-          knowledge in Computer Science. I enjoy attending hackathons, where an
-          idea can materialise into a working project in just a few hours. I
-          have previously been fully financed in two programming
-          competitions: <Link href="http://visionhack.misis.ru/">VisionHack
-          </Link> in Moscow and <Link href="https://hackdays.fcbayern.com/">FC
-          Bayern HackDays</Link>. Additionally, I have
-          worked as a freelancer on several projects.
-        </Section>
-        <Section
-          order={orient === 'landscape' ? 0 : 0}
-          src="climbing.jpeg"
-          alt="climbing">
-          After completing my studies at Imperial College, I
-          joined <Link href="https://open-cosmos.com">Open Cosmos
-          Ltd</Link>. There I led the Front End development for the company,
-          focusing on creating an intuitive and robust platform. Now, I am a
-          Mobile Developer at <Link href="https://www.wetipp.com">WeTipp</Link>.
-          I am proficient in React and Angular. During my free time, I love
-          doing mountaineering sports. If you want to join me, you’ll probably
-          find me in the mountains.
-        </Section>
-        <Social></Social>
+        <Social />
       </div>
     );
   }
