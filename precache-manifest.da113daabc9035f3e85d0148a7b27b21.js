3357a7003ae4546a489569fec0ca284afd9f695e
Sebastia Ramon <sebastiaramon_1994@hotmail.com>
2020-09-14 01:18:26 +0200

    Updates

---
+++ precache-manifest.da113daabc9035f3e85d0148a7b27b21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfb63748f44bdee141bc24bbe23074c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b2079ea6a21a043d736",
+    "url": "/static/css/main.31d5735c.chunk.css"
+  },
+  {
+    "revision": "3df8f29d706de5ca9385",
+    "url": "/static/js/2.fece435d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fece435d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b2079ea6a21a043d736",
+    "url": "/static/js/main.07847d29.chunk.js"
+  },
+  {
+    "revision": "f55e2f6b36cea2bffeb8",
+    "url": "/static/js/runtime-main.c01c5e02.js"
+  }
+]);
